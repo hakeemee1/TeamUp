ac8db13ea291f06a1f5e1f06c53f5734a24a2f01
Hakimi keerai <hakeemee222@gmail.com>
2023-10-02 13:56:59 +0700

    register insert new user in db

--- server/server.js
+++ server/server.js
@@ -1,33 +1,56 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors')
+const express = require('express')
+const mysql = require('mysql2/promise')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const bcrypt = require('bcrypt')
 
-const app = express();
-const port = 8888;
+const app = express()
+app.use(express.json())
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:8888']
+}))
+app.use(cookieParser())
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}))
 
-// Create a MySQL connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'teamup',
-});
+const port = 8000
+const secret = 'mysecret'
 
-db.connect((err) => {
-  if (err) {
-    console.error('MySQL connection failed:', err);
-  } else {
-    console.log('Connected to MySQL');
+let conn = null
+
+// function init connection mysql
+const initMySQL = async () => {
+  conn = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'teamup'
+  })
+}
+
+/* เราจะแก้ไข code ที่อยู่ตรงกลาง */
+app.post('/api/register', async (req, res) => {
+  const { email, password} = req.body
+  const userData ={
+    email,
+    password
   }
-});
+  const [results] = await conn.query('INSERT INTO users SET ?', userData)
+  res.json({
+    message : "Insert ok",
+    results
+  })
+})
 
-// Define your API routes for registration and login here
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Listen
+app.listen(port, async () => {
+  await initMySQL()
+  console.log('Server started at port 8000')
+})
