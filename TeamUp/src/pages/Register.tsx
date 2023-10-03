9d9ea927cbd5189c9a5b5e711631df755790865c
Hakimi keerai <hakeemee222@gmail.com>
2023-10-03 15:46:31 +0700

    axios to db (function)

--- TeamUp/src/pages/Register.tsx
+++ TeamUp/src/pages/Register.tsx
@@ -1,59 +1,75 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function Register() {
+const Register = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    // Add more fields as needed
+  });
 
-  const register = async () => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
     try {
-      const email = document.querySelector('input[name = email]').value 
-      const password = document.querySelector('input[name = password]').value
-      const response = await axios.post('http://localhost:8000/api/register' , {
-        email,
-        password
-      })
+      // Send a POST request to the server
+      await axios.post('http://localhost:8000/api/register', formData);
+
+      // Handle success (e.g., show a success message)
+      console.log('Data submitted successfully');
     } catch (error) {
-      console.log('error', error)
-
+      // Handle error (e.g., display an error message)
+      console.error('Error submitting data:', error);
     }
-  }
+  };
+
   return (
-    <div className="bg-gray-100 h-screen flex items-center justify-center">
-      <div className="bg-white p-8 rounded shadow-md">
-        <h2 className="text-2xl font-semibold mb-4">Create an account</h2>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="border rounded w-full py-2 px-3"
-            type="email"
-            name="email"
-            placeholder="Email"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="border rounded w-full py-2 px-3"
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
-        </div>
-        <div className="text-center">
-          <button  className="bg-green-400 text-white py-2 px-4 rounded hover:bg-blue-600">
-            Register
-          </button>
-         
-        </div>
+    <form onSubmit={handleSubmit} className="bg-blue-200 p-6 rounded-lg shadow-md max-w-md mx-auto mt-56">
+  <div className="mb-4">
+    <h1 className=' text-center text-3xl font-semibold mb-10'>Register</h1>
+    <label className="block text-blue-600 text-sm font-semibold mb-2" htmlFor="email">
+      Email:
+    </label>
+    <input
+      className="border border-blue-400 rounded w-full py-2 px-3 focus:outline-none focus:border-blue-600"
+      type="email"
+      name="email"
+      value={formData.email}
+      onChange={handleChange}
+      required
+    />
+  </div>
+  <div className="mb-4">
+    <label className="block text-blue-600 text-sm font-semibold mb-2" htmlFor="password">
+      Password:
+    </label>
+    <input
+      className="border border-blue-400 rounded w-full py-2 px-3 focus:outline-none focus:border-blue-600"
+      type="password"
+      name="password"
+      value={formData.password}
+      onChange={handleChange}
+      required
+    />
+  </div>
+  {/* Add more input fields as needed */}
+  <button
+    type="submit"
+    className="bg-blue-600 hover:bg-green-500 text-white py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105"
+  >
+    Submit
+  </button>
+</form>
 
-        <hr className="my-6 border-t" />
-        <p className=' text-center text-gray-400'>created by hakeemee1</p>
-      </div>
-    </div>
   )
-}
+};
 
-export default Register
+export default Register;
